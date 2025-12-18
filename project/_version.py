a1aa616f2885f2794f428ededb670a16288d6ba9
mik2003 <62058516+mik2003@users.noreply.github.com>
2025-12-18 23:20:58 +0100

    Added LICENSE

--- project/_version.py
+++ project/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev0+gabd804130.d20251218'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'gabd804130.d20251218')
+__version__ = version = '0.1.1.dev2+g63d9c9d22.d20251218'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev2', 'g63d9c9d22.d20251218')
 
-__commit_id__ = commit_id = 'gabd804130'
+__commit_id__ = commit_id = 'g63d9c9d22'
