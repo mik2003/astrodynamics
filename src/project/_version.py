0f7da41d9dd4a70504644a33275c4c857a38b0ce
mik2003 <62058516+mik2003@users.noreply.github.com>
2025-12-30 14:53:38 +0100

    Updated version

--- src/project/_version.py
+++ src/project/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.2.1.dev0+g7adf6a711.d20251230'
-__version_tuple__ = version_tuple = (0, 2, 1, 'dev0', 'g7adf6a711.d20251230')
+__version__ = version = '0.2.2.dev0+ga5b69f84b.d20251230'
+__version_tuple__ = version_tuple = (0, 2, 2, 'dev0', 'ga5b69f84b.d20251230')
 
-__commit_id__ = commit_id = 'g7adf6a711'
+__commit_id__ = commit_id = 'ga5b69f84b'
