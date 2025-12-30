e82a3f826a318a879b6fcfe5a3355720275bfc24
mik2003 <62058516+mik2003@users.noreply.github.com>
2025-12-30 14:29:04 +0100

    Added tag

--- src/project/_version.py
+++ src/project/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.5.dev13+g6ef57282c.d20251230'
-__version_tuple__ = version_tuple = (0, 1, 5, 'dev13', 'g6ef57282c.d20251230')
+__version__ = version = '0.2.1.dev0+g7adf6a711.d20251230'
+__version_tuple__ = version_tuple = (0, 2, 1, 'dev0', 'g7adf6a711.d20251230')
 
-__commit_id__ = commit_id = 'g6ef57282c'
+__commit_id__ = commit_id = 'g7adf6a711'
