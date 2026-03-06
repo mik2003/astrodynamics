3e16706df6b7a300cc3d607a3be0c58cc19d49bf
mik2003 <62058516+mik2003@users.noreply.github.com>
2026-03-06 18:54:02 +0100

    Updated LICENSE in metadata

--- src/project/__init__.py
+++ src/project/__init__.py
@@ -5,3 +5,7 @@
 from ._version import __version__
 
 __all__ = ["__version__"]
+
+__author__ = "Michelangelo Secondo"
+__email__ = "michelangelo@secondo.aero"
+__license__ = "AGPL-3.0-or-later AND CC0-1.0"
